e54e101541d684fc4abcca4b2173acee41bc424c
root <root@localhost.localdomain>
2019-09-25 23:21:52 +0100

    docsigner

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SignatureCanvas from 'react-signature-canvas';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Boye put me here</div>
+      <SignatureCanvas 
+      penColor='brown'
+      canvasProps={{width: 500, height: 200, className: 'sigCanvas'}}
+      backgroundColor = 'rgb(240, 242, 245)'
+      />
     </div>
   );
 }
