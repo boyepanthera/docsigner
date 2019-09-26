1793c31b2226d8638f1996a03e3515948dfa7254
root <root@localhost.localdomain>
2019-09-26 04:57:03 +0100

    edit index html

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import './App.css';
 import SignatureCanvas from 'react-signature-canvas';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <div>Boye put me here</div>
-      <SignatureCanvas 
-      penColor='brown'
-      canvasProps={{width: 500, height: 200, className: 'sigCanvas'}}
-      backgroundColor = 'rgb(240, 242, 245)'
-      />
+    <div className="container">
+      <div className='text-center'>Draw your signature here!</div>
+      <div className='m-5'>
+        <SignatureCanvas 
+        penColor='black'
+        canvasProps={{width: 500, height: 200, className: 'sigCanvas'}}
+        backgroundColor = 'rgb(240, 242, 245)'
+        />
+      </div>
+      <div>
+        <button className='btn btn-success'>Save</button> 
+        <button className='btn btn-danger float-right'>Delete</button>
+      </div>
     </div>
   );
 }
